97d8b54f2a11e1d1e3bda0beb4c4a2eef1409207
Ricardo Sabatino <rick@coderick.com>
2016-10-15 12:13:18 -0400

    agregados login de FB y GOOGLE a /index

--- controllers/home.js
+++ controllers/home.js
@@ -1,10 +1,52 @@
 /*jshint esversion: 6 */
+const async = require('async');
+const passport = require('passport');
+const User = require('../models/User');
 /**
  * GET /
  * Home page.
  */
 exports.index = (req, res) => {
   res.render('home', {
-    title: 'Home'
+    title: 'Inicio'
+  });
+};
+
+/**
+ * POST /signup
+ * Create a new local account.
+ */
+exports.postSignup = (req, res, next) => {
+  req.assert('email', 'Email is not valid').isEmail();
+  req.assert('password', 'Contraseña debe ser de al menos 8 caracteres').len(8);
+  req.sanitize('email').normalizeEmail({ remove_dots: false });
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('/signup');
+  }
+
+  const user = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  User.findOne({ email: req.body.email }, (err, existingUser) => {
+    if (err) { return next(err); }
+    if (existingUser) {
+      req.flash('errors', { msg: 'Este correo ya se encuentra registrado.' });
+      return res.redirect('/signup');
+    }
+    user.save((err) => {
+      if (err) { return next(err); }
+      req.logIn(user, (err) => {
+        if (err) {
+          return next(err);
+        }
+        res.redirect('/videos');
+      });
+    });
   });
 };
