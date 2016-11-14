401b555dc7ee12b70ed2fb3109321569e23f4c86
Ricardo Sabatino <rick@coderick.com>
2016-11-14 07:42:23 -0400

    temporal disabling of search for maintenance

--- controllers/search.js
+++ controllers/search.js
@@ -8,16 +8,16 @@ const User = require('../models/User');
 exports.searching= (req, res) => {
   //llevo el user input a minuscula
   let busqueda = _.toLower(req.query.search);
-  //let resultados = _.filter(videodb, function(item) {
-    //llevo el indice de busqueda a minuscula tambien para que sean compatibles
-    let tema = _.toLower(item.theme);
-    let bloque = _.toLower(item.block);
-    let titulo = _.toLower(item.title);
-
-    if (tema.indexOf(busqueda)>= 0 || bloque.indexOf(busqueda)>= 0 || titulo.indexOf(busqueda)>= 0){
-      return item;
-    }
-  });
+  // let resultados = _.filter(videodb, function(item) {
+  //   llevo el indice de busqueda a minuscula tambien para que sean compatibles
+  //   let tema = _.toLower(item.theme);
+  //   let bloque = _.toLower(item.block);
+  //   let titulo = _.toLower(item.title);
+  //
+  //   if (tema.indexOf(busqueda)>= 0 || bloque.indexOf(busqueda)>= 0 || titulo.indexOf(busqueda)>= 0){
+  //     return item;
+  //   }
+  // });
 
   let hits = resultados.length;
 
